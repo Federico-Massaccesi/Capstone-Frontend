a581b608e1d74560e320d97ef715ce338937d355
Federico-Massaccesi <fede99ita26@gmail.com>
2024-07-16 11:58:33 +0200

    chatbot istruito e stilizzato chat box

--- src/app/main-component/chat/chat.component.ts
+++ src/app/main-component/chat/chat.component.ts
@@ -9,8 +9,9 @@ import { ChatService } from './chat.service';
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
-  messages: string[] = [];
+  messages: { text: string, isBot: boolean }[] = [];
   userMessage: string = '';
+  isChatExpanded: boolean = false;
 
   constructor(private chatService: ChatService) { }
 
@@ -19,11 +20,28 @@ export class ChatComponent {
 
   sendMessage() {
     if (this.userMessage.trim().length > 0) {
+      this.messages.push({ text: `User: ${this.userMessage}`, isBot: false });
       this.chatService.sendMessage(this.userMessage).subscribe(response => {
-        this.messages.push(`User: ${this.userMessage}`);
-        this.messages.push(`Bot: ${response.message}`);
+        this.displayBotMessage(response.message);
         this.userMessage = '';
       });
     }
+  }
+
+  displayBotMessage(message: string) {
+    let index = 0;
+    const interval = setInterval(() => {
+      if (index < message.length) {
+        const currentMessage = this.messages[this.messages.length - 1];
+        if (currentMessage && currentMessage.isBot) {
+          currentMessage.text += message.charAt(index);
+        } else {
+          this.messages.push({ text: message.charAt(index), isBot: true });
+        }
+        index++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 20);
   }
 }
